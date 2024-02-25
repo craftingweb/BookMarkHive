82df917788889d9914f03713069af3fdc6dc1e72
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-02-25 18:01:16 -0500

    local storage & delete bookmark

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const bookmarkForm = document.getElementById("bookmark-form");
 const websiteNameEl = document.getElementById("website-name");
 const websiteUrlEl = document.getElementById("website-url");
 const bookmarksContainer = document.getElementById("bookmarks-container");
+const btn = document.getElementById("btn");
+
+let bookmarks = [];
 
 // Show Modal, Focus on Input
 function showModal() {
@@ -18,6 +21,8 @@ modalShow.addEventListener("click", showModal);
 modalClose.addEventListener("click", () =>
   modal.classList.remove("show-modal")
 );
+
+btn.addEventListener("click", () => modal.classList.remove("show-modal"));
 
 window.addEventListener("click", (e) =>
   e.target === modal ? modal.classList.remove("show-modal") : false
@@ -35,6 +40,62 @@ function validate(nameValue, urlValue) {
   return true;
 }
 
+// Building Bookmarks DOM
+function buildBookmarks() {
+  bookmarksContainer.textContent = "";
+  bookmarks.forEach((bookmark) => {
+    const { name, url } = bookmark;
+    const item = document.createElement("div");
+    item.classList.add("item");
+    const closeIcon = document.createElement("i");
+    closeIcon.classList.add("fas", "fa-times");
+    closeIcon.setAttribute("title", "Delete");
+    closeIcon.setAttribute("onclick", `deleteBookmark('${url}')`);
+    const linkInfo = document.createElement("div");
+    linkInfo.classList.add("name");
+    const favicon = document.createElement("img");
+    favicon.setAttribute(
+      "src",
+      `https://s2.googleusercontent.com/s2/favicons?domain=${url}`
+    );
+    favicon.setAttribute("alt", "Favicon");
+    const link = document.createElement("a");
+    link.setAttribute("href", `${url}`);
+    link.setAttribute("target", "_blank");
+    link.textContent = name;
+    linkInfo.append(favicon, link);
+    item.append(closeIcon, linkInfo);
+    bookmarksContainer.appendChild(item);
+  });
+}
+
+// Fetch data
+function fetchBookmarks() {
+  if (localStorage.getItem("bookmarks")) {
+    bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+  } else {
+    bookmarks = [
+      {
+        name: "Vitali Design",
+        url: "https://vitaliyprymak.com",
+      },
+    ];
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  }
+  buildBookmarks();
+}
+
+// Delete Bookmark
+function deleteBookmark(url) {
+  bookmarks.forEach((bookmark, i) => {
+    if (bookmark.url === url) {
+      bookmarks.splice(i, 1);
+    }
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+    fetchBookmarks();
+  });
+}
+
 // Handle Data from Form
 function storeBookmark(e) {
   e.preventDefault();
@@ -50,7 +111,20 @@ function storeBookmark(e) {
   if (!validate(nameValue, urlValue)) {
     return false;
   }
+  let bookmark = {
+    name: nameValue,
+    url: urlValue,
+  };
+  bookmarks.push(bookmark);
+  console.log(bookmarks);
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  fetchBookmarks();
+  bookmarkForm.reset();
+  websiteNameEl.focus;
 }
 
 // Event Listener
 bookmarkForm.addEventListener("submit", storeBookmark);
+
+// On load
+fetchBookmarks();
